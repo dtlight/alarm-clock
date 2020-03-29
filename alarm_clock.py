61d8908a28d900ab736a85660ee75067dfb31f8d
Dave Light <mail@davelight.io>
2020-03-29 23:36:52 +0100

    add autoAlarm to alarm_clock.py

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,3 +1,10 @@
 
 def autoAlarm(day, noUni):
-    return 5
+	weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+	weekends = ["Saturday","Sunday"]
+	if day in weekends:
+		return "9:00"
+	elif day in weekdays and noUni:
+		return "9:00"
+	else:
+		return "7:45" if day == "Wednesday" or day == "Thursday" else "6:00"
