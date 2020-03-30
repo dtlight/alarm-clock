6d6c781ec3ab05935b633a6a0b863a1c211222b1
Dave Light <mail@davelight.io>
2020-03-30 01:00:12 +0100

    add notAtUni()

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,3 +1,4 @@
+import datetime
 
 def autoAlarm(day, noUni):
 	weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
@@ -8,3 +9,20 @@ def autoAlarm(day, noUni):
 		return "9:00"
 	else:
 		return "7:45" if day == "Wednesday" or day == "Thursday" else "6:00"
+
+def notAtUni(date):
+    '''
+    Check whether or not I am at university. 
+    '''
+    termMonths = ["Jan", "Feb", "Mar", "May", "Sep", "Oct", "Nov"]
+    if date.strftime("%b") in termMonths:
+        return False
+    else:
+        return True
+
+if __name__ == '__main__':
+    today = datetime.datetime.now()
+    day = today.strftime("%A")
+    atUni = notAtUni(today)
+    alarmTime = autoAlarm(day, atUni)
+    print ("Alarm is set for", alarmTime)
